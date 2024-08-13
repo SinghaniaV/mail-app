2a7d179fef7a20b5cb6a09f00c23b3fa88927b8e
Vishal Singhania <vishsinghania@outlook.com>
2024-08-13 21:54:55 +0530

    typo

--- mailapp/asgi.py
+++ mailapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project3 project.
+ASGI config for mailapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- mailapp/wsgi.py
+++ mailapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project3 project.
+WSGI config for mailapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
