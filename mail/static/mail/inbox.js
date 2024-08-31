da283aa9879d9c8a5c80332754e3c9907c606140
Vishal Singhania <vishsinghania@outlook.com>
2024-09-01 00:46:34 +0530

    implemented load_mailbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -33,11 +33,49 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
+
+  // Store current view in variable
+  current_view = mailbox;
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#single-email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Add the email list container
+  if(document.getElementById("emails-table") == null){
+    const element = document.createElement('div');
+    element.id = "emails-table";
+
+  document.querySelector('div[id="emails-view"]').append(element);
+  }
+
+  let tableBody = document.querySelector('div[id="emails-table"]');
+
+  // Clear old emails
+  tableBody.innerHTML = '';
+
+  // Fetch the emails
+  fetch(`emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => processEmails(emails));
+
+  let processEmails = (emails) => {
+    emails.forEach(function (email){
+      console.log(email);
+      const temp_element = document.createElement('a');
+      temp_element.classList.add('email-link');
+      temp_element.innerHTML= `
+            <div class="${email.read?'read':'unread'} row border m-1">
+                <div class="col-sm-3 email-sender">${email.sender}</div>
+                <div class="col-sm-6 email-title">${email.subject}</div>
+                <div class="col-sm-3 email-date">${email.timestamp}</div>
+            </div>`;
+      tableBody.append(temp_element);
+      temp_element.addEventListener('click', ()=>display_email(email.id));
+    })
+  }
 }
