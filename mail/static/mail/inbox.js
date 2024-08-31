e3f1dd92383205b03e9a3de2aaf72e4ded5118eb
Vishal Singhania <vishsinghania@outlook.com>
2024-09-01 00:44:58 +0530

    implemented compose_email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -14,12 +14,22 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // Turn off default button send behaviour
+  document.querySelector('#compose-form').addEventListener("submit", function(evt){
+    evt.preventDefault();
+    //window.history.back();
+  }, true);
+
+  // Add event listener to send email button
+  document.querySelector('input[type="submit"]').addEventListener('click', send_email);
 }
 
 function load_mailbox(mailbox) {
